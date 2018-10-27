ca1ae7adcdcd38e98e2549833eccf3efdf5b0f9f
tshaiman <tomer.shaiman@gmail.com>
2018-10-27 11:14:46 +0300

    sd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-'use strict';
+
 
 const express = require('express');
-
+var myaddr  = "";
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world v-2 !\n');
+  res.send('Hello world v-2! \n');
 });
 
 app.listen(PORT, HOST);
